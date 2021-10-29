788ae23e2e78969605c373facc78736d740fd58a
kr-prince <nphard12@gmail.com>
2021-10-30 01:24:22 +0530

    fix indent

--- ImageClassifier/download_image.py
+++ ImageClassifier/download_image.py
@@ -9,28 +9,28 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
 
 def download_image():
-	try:
-		# Clear the download folder
-		img_dir = './download'
-		for file in os.listdir(img_dir):
-			os.remove(os.path.join(img_dir, file))
+  try:
+    # Clear the download folder
+    img_dir = './download'
+    for file in os.listdir(img_dir):
+      os.remove(os.path.join(img_dir, file))
 
-		# Adding information about user agent
-		opener=urllib.request.build_opener()
-		opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')]
-		urllib.request.install_opener(opener)
-		
-		# Download the image in the download folder
-		image_url = title
-		file_name = image_url.split('/')[-1]
-		(file_path, response) = urllib.request.urlretrieve(image_url, os.path.join(img_dir, file_name))
+    # Adding information about user agent
+    opener=urllib.request.build_opener()
+    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    
+    # Download the image in the download folder
+    image_url = title
+    file_name = image_url.split('/')[-1]
+    (file_path, response) = urllib.request.urlretrieve(image_url, os.path.join(img_dir, file_name))
 
-		# Send the file_path to Kafka
-		producer.send('img_file_path', file_path.encode('utf-8'))
+    # Send the file_path to Kafka
+    producer.send('img_file_path', file_path.encode('utf-8'))
 
-		st.success("Image downloaded : %s " %file_path)
-	except Exception as ex:
-		st.error("Download failed : %s " %str(ex))
+    st.success("Image downloaded : %s " %file_path)
+  except Exception as ex:
+    st.error("Download failed : %s " %str(ex))
 
 
 st.title('Image Classifier')
